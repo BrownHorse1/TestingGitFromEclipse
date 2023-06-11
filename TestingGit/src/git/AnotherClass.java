7d64180b644325fdb7fc95e0b0f368cb5b842876
Lenovo <Lenovo@192.168.99.24>
2023-06-11 21:22:41 +0630

    Commit to TeamB

--- TestingGit/src/git/AnotherClass.java
+++ TestingGit/src/git/AnotherClass.java
@@ -4,7 +4,7 @@ public class AnotherClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Pull me please");
+		System.out.println("Pull me please TeamB");
 
 	}
 
